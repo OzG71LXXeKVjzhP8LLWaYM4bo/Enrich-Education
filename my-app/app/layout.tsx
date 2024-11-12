e3cf546f9c416ff25aefd5d3ff4fceab9fc2ea8f
OzG71LXXeKVjzhP8LLWaYM4bo <177896519+OzG71LXXeKVjzhP8LLWaYM4bo@users.noreply.github.com>
2024-11-12 23:46:13 +1100

    a

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { Header } from "@/components/ui/header";
 import { Footer } from "@/components/ui/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <Header />
         {children}
         <Footer />
